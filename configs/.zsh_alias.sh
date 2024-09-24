43e635874971f321a1d6289f16002df448a6ff9d
Yeeb <47221467+Yeeb1@users.noreply.github.com>
2024-09-24 11:39:18 +0200

    Update .zsh_alias.sh

--- configs/.zsh_alias.sh
+++ configs/.zsh_alias.sh
@@ -1,45 +1,41 @@
-
 ########### General Alias Configuration #############
-alias ls='lsd'
-alias ll='lsd -la'
-alias la='lsd -A'
-alias l='lsd -CF'
-alias f='find .'
-alias t='tree .'
-alias xc="xclip -selection clipboard"
+alias ls='lsd' # lsd instead of ls
+alias ll='lsd -la' # listing all files, including hidden ones, with detailed info
+alias la='lsd -A' # listing all files except . and ..
+alias l='lsd -CF' # compact listing of files in columns
+alias f='find .' # search files and directories from the current directory
+alias t='tree .' # display the directory structure in tree format
+alias xc="xclip -selection clipboard" # Alias for copying to the clipboard
 ########### General Alias Configuration #############
-
 
 ########### General CTF Aliases #############
-alias dockershell="sudo docker run --rm -i -t --entrypoint=/bin/bash"
-alias dockershellsh="sudo docker run --rm -i -t --entrypoint=/bin/sh"
-alias ntlm.pw='function _ntlm(){ curl https://ntlm.pw/$1; }; _ntlm'
-alias bat="batcat"
-alias peas='wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh -O p'
-alias mkctf='mkdir files serve loot tools'
-alias mkserve='rm -rf s && ln -s /opt/arsenal/serve s'
+alias dockershell="sudo docker run --rm -i -t --entrypoint=/bin/bash" # Runs a Docker container with a bash shell
+alias dockershellsh="sudo docker run --rm -i -t --entrypoint=/bin/sh" # Runs a Docker container with a sh shell
+alias ntlm.pw='function _ntlm(){ curl https://ntlm.pw/$1; }; _ntlm' # Fetches NTLM hashes from ntlm.pw for a given value
+alias bat="batcat" # Alias to use batcat instead of cat
+alias peas='wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh -O p' # Downloads linpeas.sh
+alias mkctf='mkdir files serve loot tools' # Creates commonly used directories for CTFs
+alias mkserve='rm -rf s && ln -s /opt/arsenal/serve s' # Alias to reset and relink the 'serve' folder
 ########### General CTF Aliases #############
 
-########### HTB Aliases #############
-alias machines="cd /home/kali/ctf/htb/machines"
-alias fort="cd /home/kali/ctf/htb/fortress"
-alias challenges="cd /home/kali/ctf/htb/challenges"
-alias ctf="cd /home/kali/ctf"
-alias htbvpn="sudo openvpn /home/kali/ovpn/lab_Yeeb.ovpn"
-alias relvpn="sudo openvpn /home/kali/ovpn/competitive_Yeeb.ovpn"
-alias fortvpn="sudo openvpn /home/kali/ovpn/fortresses_Yeeb.ovpn"
-alias endvpn="sudo openvpn /home/kali/ovpn/endgames_Yeeb.ovpn"
-alias prolabsvpn="sudo openvpn /home/kali/ovpn/pro_labs_Yeeb.ovpn"
-alias htb='cd /home/kali/ctf/htb'
-alias zephyr="/home/kali/ctf/htb/prolabs/zephyr"
-alias dante='/home/kali/ctf/htb/prolabs/dante'
-########### HTB Aliases #############
+########### Vulnlab Aliases #############
+alias vuln="cd /home/kali/ctf/vulnlab" # Navigate to Vulnlab directory
+alias vmachines="cd /home/kali/ctf/vulnlab/machines" # Navigate to Vulnlab machines directory
+alias chains="cd /home/kali/ctf/vulnlab/chains" # Navigate to Vulnlab chains directory
+alias rtlabs="cd /home/kali/ctf/vulnlab/rtl" # Navigate to Vulnlab RTL directory
+alias vlvpn="sudo openvpn /home/kali/ovpn/692063591469416498_aws.ovpn" # Start Vulnlab VPN
+alias rtvpn="sudo openvpn /home/kali/ovpn/692063591469416498_rtl01.ovpn" # Start RTL VPN
+########### Vulnlab Aliases #############
 
-########### Vulnlab Aliases #############
-alias vuln="cd /home/kali/ctf/vulnlab"
-alias vmachines="cd /home/kali/ctf/vulnlab/machines"
-alias chains="cd /home/kali/ctf/vulnlab/chains"
-alias rtlabs="cd /home/kali/ctf/vulnlab/rtl"
-alias vlvpn="sudo openvpn /home/kali/ovpn/692063591469416498_aws.ovpn"
-alias rtvpn="sudo openvpn /home/kali/ovpn/692063591469416498_rtl01.ovpn"
-########### Vulnlab Aliases #############
+########### HTB Aliases #############
+alias machines="cd /home/kali/ctf/htb/machines" # Navigate to the HTB machines directory
+alias fort="cd /home/kali/ctf/htb/fortress" # Navigate to the HTB fortress directory
+alias challenges="cd /home/kali/ctf/htb/challenges" # Navigate to the HTB challenges directory
+alias ctf="cd /home/kali/ctf" # Navigate to the root CTF directory
+alias htbvpn="sudo openvpn /home/kali/ovpn/lab_Yeeb.ovpn" # Start the HTB VPN
+alias relvpn="sudo openvpn /home/kali/ovpn/competitive_Yeeb.ovpn" # Start the release VPN
+alias fortvpn="sudo openvpn /home/kali/ovpn/fortresses_Yeeb.ovpn" # Start the fortress VPN
+alias endvpn="sudo openvpn /home/kali/ovpn/endgames_Yeeb.ovpn" # Start the endgames VPN
+alias prolabsvpn="sudo openvpn /home/kali/ovpn/pro_labs_Yeeb.ovpn" # Start the pro labs VPN
+alias htb='cd /home/kali/ctf/htb' # Navigate to HTB directory
+########### HTB Aliases #############
